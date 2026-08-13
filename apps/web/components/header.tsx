97c0ac3741b4566128396ecd60120edb5d30f24e
talal alghazal <talalalghazal55@gmail.com>
2026-08-13 10:37:54 +0300

    feat: create a sheet to open navigation when on small screens

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -1,21 +1,29 @@
 "use client"
 import { Button, buttonVariants } from "@workspace/ui/components/button"
 import { HugeiconsIcon } from "@hugeicons/react"
-import { ArrowUpLeft } from "@hugeicons/core-free-icons"
+import { ArrowUpLeft, Menu01Icon } from "@hugeicons/core-free-icons"
 import Link from "next/link"
-import React from "react"
+import React, { useState } from "react"
 import { cn } from "@workspace/ui/lib/utils"
 import { useLenis } from "@/components/providers/lenis-provider"
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@workspace/ui/components/sheet"
 
 const navLinks = [
-  { label: "خدماتنا", href: "#services" },
+  { label: "خدماتنا", href: "/#services" },
   { label: "أعمالنا", href: "/portfolio" },
-  { label: "من نحن؟", href: "#who-are-we" },
+  { label: "من نحن؟", href: "/#who-are-we" },
 ]
 
 export default function Header() {
   const { scrollTo } = useLenis()
-
+  const [open, setOpen] = useState(false)
   function handleNavClick(
     e: React.MouseEvent<HTMLAnchorElement>,
     href: string
@@ -30,19 +38,22 @@ export default function Header() {
     <header className="fixed z-99 flex w-full items-center justify-between border-b border-accent bg-background/60 p-5 backdrop-blur-3xl">
       <a
         href="#"
-        onClick={(e) => { e.preventDefault(); scrollTo(0) }}
-        className="font-thmanyah-heading text-2xl font-bold text-foreground cursor-pointer"
+        onClick={(e) => {
+          e.preventDefault()
+          scrollTo(0)
+        }}
+        className="cursor-pointer font-thmanyah-heading text-2xl font-bold text-foreground"
       >
         <span className="text-primary">AR</span> Marketing
       </a>
-      <nav className="flex items-center gap-10">
+      <nav className="flex items-center gap-10 not-md:hidden md:block">
         {navLinks.map(({ label, href }) => (
           <a
             key={href}
             href={href}
             onClick={(e) => handleNavClick(e, href)}
             className={cn(
-              "font-thmanyah-subheading-sans text-foreground! cursor-pointer",
+              "cursor-pointer font-thmanyah-subheading-sans text-foreground!",
               buttonVariants({ variant: "link" })
             )}
           >
@@ -64,6 +75,53 @@ export default function Header() {
           <HugeiconsIcon strokeWidth={2} icon={ArrowUpLeft}></HugeiconsIcon>
         </Link>
       </nav>
+      <Sheet modal open={open} onOpenChange={(open) => setOpen(open)}>
+        <SheetTrigger
+          className={"md:hidden"}
+          render={
+            <Button variant={"ghost"} size={"icon-sm"}>
+              <HugeiconsIcon icon={Menu01Icon} />
+            </Button>
+          }
+        />
+        <SheetContent side="top" className={"z-99"} dir="rtl">
+          <SheetHeader>
+            <SheetTitle>
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault()
+                  setOpen(false)
+                  scrollTo(0)
+                }}
+                className="cursor-pointer font-thmanyah-heading text-2xl font-bold text-foreground"
+              >
+                <span className="text-primary">AR</span> Marketing
+              </a>
+            </SheetTitle>
+          </SheetHeader>
+          <SheetFooter>
+            <div className="flex flex-col gap-4">
+              {navLinks.map(({ label, href }) => (
+                <a
+                  key={href}
+                  href={href}
+                  onClick={(e) => {
+                    handleNavClick(e, href)
+                    setOpen(false)
+                  }}
+                  className={cn(
+                    "cursor-pointer font-thmanyah-subheading-sans text-foreground!",
+                    buttonVariants({ variant: "link" })
+                  )}
+                >
+                  {label}
+                </a>
+              ))}
+            </div>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </header>
   )
 }
